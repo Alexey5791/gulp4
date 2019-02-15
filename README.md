28e2d79003453da6edcdef716db8f56c4cb54cf9
Alexey <alex7pronin@gmail.com>
2019-02-15 11:34:27 +0500

    add task styles

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 gulp template  
 # How to use  
 npm i gulp-cli -g  
-npm i gulp -d
+npm i gulp gulp-sass gulp-clean-css gulp-autoprefixer -d
--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,17 @@
 const gulp = require('gulp');
-
-
-function styles(){
-
-}
-function scripts(){
-
-}
-
-gulp.task('styles', styles);
-gulp.task('scripts', scripts);
+const sass = require('gulp-sass');
+const cleanCSS = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
+ 
+sass.compiler = require('node-sass');
+ 
+gulp.task('styles', function () {
+  return gulp.src('./src/sass/**/*.sass')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+    }))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('./dist/css'));
+});
